1b418e134ed9416ec5cd89ba4887b91b3c1174ae
Luis Angel <santosangel748@gmail.com>
2020-10-26 18:31:05 -0600

    protegiendo rutas con Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidoComponent } from './components/protegido/protegido.component';
+import { AuthGuardGuard } from './services/auth-guard.guard';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'protegido',
    component: ProtegidoComponent,
-   canActivate: [AuthGuard]
+   canActivate: [AuthGuardGuard]
   },
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
